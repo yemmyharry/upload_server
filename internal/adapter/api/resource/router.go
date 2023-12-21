a82867c7add6a407e2d2e50211520d8f623b1bef
yemmyharry <yemmyharry@gmail.com>
2023-12-21 12:43:19 +0100

    Added upload file endpoint

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -7,5 +7,6 @@ import (
 func (s *HTTPHandler) Routes(router *gin.Engine) {
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/healthcheck", s.HealthCheck)
+	apirouter.POST("/upload", s.UploadFile)
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
