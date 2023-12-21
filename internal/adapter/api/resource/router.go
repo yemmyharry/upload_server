e8567a7399f3e17ec708d68d550ec34087786a82
yemmyharry <yemmyharry@gmail.com>
2023-12-21 13:08:31 +0100

    Added get files endpoint

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -8,5 +8,6 @@ func (s *HTTPHandler) Routes(router *gin.Engine) {
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/healthcheck", s.HealthCheck)
 	apirouter.POST("/upload", s.UploadFile)
+	apirouter.GET("files", s.GetAllFiles)
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
