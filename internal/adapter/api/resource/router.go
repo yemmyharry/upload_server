87b35ba375b192633fd14950c9ada0cf4c5271ef
yemmyharry <yemmyharry@gmail.com>
2023-12-21 13:38:45 +0100

    Added download file endpoint

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -8,6 +8,7 @@ func (s *HTTPHandler) Routes(router *gin.Engine) {
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/healthcheck", s.HealthCheck)
 	apirouter.POST("/upload", s.UploadFile)
-	apirouter.GET("files", s.GetAllFiles)
+	apirouter.GET("/files", s.GetAllFiles)
+	apirouter.GET("/download/:filename", s.DownloadFile)
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
