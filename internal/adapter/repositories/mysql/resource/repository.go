44261949e8ce1790bd3b4c7c46be3019ea7ff62f
yemmyharry <yemmyharry@gmail.com>
2023-12-21 12:42:34 +0100

    Added upload file repo method

--- internal/adapter/repositories/mysql/resource/repository.go
+++ internal/adapter/repositories/mysql/resource/repository.go
@@ -1,5 +1,20 @@
 package resource
 
+import (
+	domain "upload_server/internal/core/domain/resource"
+	"upload_server/internal/core/logger"
+)
+
 func (m UploadRepositoryDB) HealthCheck() error {
+	return nil
+}
+
+func (m UploadRepositoryDB) UploadFile(file domain.File) error {
+	result := m.db.Where("name = ?", file.Name).First(&domain.File{})
+	if result.Error == nil {
+		logger.Info("File already exists")
+		return result.Error
+	}
+	m.db.Create(&file)
 	return nil
 }
