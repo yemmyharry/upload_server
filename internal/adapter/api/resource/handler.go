4fed709c25723f70c6a104b9aedd8cadd0705c0c
yemmyharry <yemmyharry@gmail.com>
2023-12-21 12:41:37 +0100

    Added upload file handler

--- internal/adapter/api/resource/handler.go
+++ internal/adapter/api/resource/handler.go
@@ -1,7 +1,12 @@
 package resource
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"path/filepath"
+	"strconv"
+	domain "upload_server/internal/core/domain/resource"
 	"upload_server/internal/core/logger"
 )
 
@@ -12,4 +17,26 @@ type HealthCheckResponse struct {
 func (s *HTTPHandler) HealthCheck(c *gin.Context) {
 	logger.Info("HealthCheck called")
 	c.JSON(200, HealthCheckResponse{Status: "OK"})
+}
+
+func (s *HTTPHandler) UploadFile(c *gin.Context) {
+	logger.Info("UploadFile called")
+	file, err := c.FormFile("file")
+	if err != nil {
+		logger.Error("No file provided")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "No file provided"})
+		return
+	}
+
+	filename := fmt.Sprintf("%s%s", strconv.FormatInt(file.Size, 10), filepath.Ext(file.Filename))
+	if err := c.SaveUploadedFile(file, filename); err != nil {
+		logger.Error("Failed to save file")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save file"})
+		return
+	}
+
+	_ = s.uploadService.UploadFile(domain.File{Name: filename})
+
+	logger.Info("File uploaded successfully")
+	c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully"})
 }
